a436db998d60beec8cd17a4fd23e291604bdc595
jvercruyssen <jvercruyssen@mail.csuchico.edu>
2016-12-06 18:39:33 -0800

    Add files via upload

--- madLibs.js
+++ madLibs.js
@@ -1,50 +1,27 @@
-
+var m1 = "";
+var m2 = "";
+var m3 = "";
+var m4 = "";
+var m5 = "";
 function submit(){
     var input1 = document.getElementById("noun1").value;
     var input2 = document.getElementById("noun2").value;
-    var input3 = document.getElementById("noun3").value;
-    var input4 = document.getElementById("noun4").value;
-    if(input1.length < 1 || input2.length < 1 || input3.length < 1 || input4.length < 1){
+    var input3 = document.getElementById("verb1").value;
+    var input4 = document.getElementById("verb2").value;
+    var input5 = document.getElementById("adj1").value;
+    if(input1.length < 1 || input2.length < 1 || input3.length < 1 || input4.length < 1 || input5.length <1){
         document.getElementById("inputError").innerHTML="You must complete all fields first";
         document.getElementById("inputError").classList.remove("hidden-message");
         document.getElementById("inputError").classList.add("shown-message");
     }
     else{
-      setCookie("n1", input1, 1);
-      setCookie("n2", input2, 1);
-      setCookie("n3", input3, 1);
-      setCookie("n4", input4, 1);
-      console.log(getCookie("n1"));
-      console.log(getCookie("n2"));
-      console.log(getCookie("n3"));
-      console.log(getCookie("n4"));
+        m1 = input1;
+        m2 = input2;
+        m3 = input3;
+        m4 = input4;
+        m5 = input5;
+        document.getElementById("story").innerHTML="There once was a " + m1 + " named Jeremy. He was best friends with a " + m2 + " named Jessica. Jeremy loved " + m3 + " while Jessica loved " + m4 + ". One day Jeremy attempted " + m4 + " with Jessica, but was unable to keep up due to his " + m5 + " " + m1 + " legs. Jessica attempted to convice Jeremy that it was okay that he was not good at " + m4 + " as she was not very good at " + m3 + " herself.";
+        document.getElementById("story").classList.remove("hidden-message");
+        document.getElementById("story").classList.add("shown-message");
     }
-}
-
-function getVariables(){
-    var noun1 = getCookie("n1").value;
-    document.getElementById("noun1").value = getCookie("n1");
-}
-
-//courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-//courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
 }
